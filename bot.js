ecf571f5ea222071562b0a8008a20172b894fc49
mutantlog <rmbayne@alumni.uwaterloo.ca>
2015-09-24 10:19:41 -0400

    Cleaning up some lint issues thanks to bithound.io

--- bot.js
+++ bot.js
@@ -2,17 +2,20 @@ var Twit  = require('twit'); // Twitter module
 
 var T = new Twit(require('./config.js'));
 
-var stream = T.stream('user')
+var stream = T.stream('user');
 
 function favTweet( tweetId ) {
 	setTimeout(function() {
 		T.post('favorites/create', {id : tweetId}, function (err, data, response) {
+			if (err) {
+				console.log("Guess there was an error: ",err);
+			}
 		}); 
 	}, Math.floor(Math.random() * 120 * 1000));
-};
+}
 
 stream.on('tweet', function (tweet) {
-	if ((tweet.user.screen_name == "ryanqnorth") && (tweet.entities.user_mentions.length == 0) && (tweet.retweeted === false) && (tweet.favorited === false) && (tweet.in_reply_to_user_id === null) && (tweet.in_reply_to_status_id === null)) {
+	if ((tweet.user.screen_name == "ryanqnorth") && (tweet.entities.user_mentions.length === 0) && (tweet.retweeted === false) && (tweet.favorited === false) && (tweet.in_reply_to_user_id === null) && (tweet.in_reply_to_status_id === null)) {
 		favTweet(tweet.id_str);
 	}
 });
